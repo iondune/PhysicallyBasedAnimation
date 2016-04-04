356a1cb0b8ae0bfdf6126c988a84383bbbc3c668
Ian Dunn <iondune@gmail.com>
2016-04-03 21:20:32 -0700

    Add associativity test

---
+++ AssociativityTest/Main.cpp
@@ -0,0 +1,91 @@
+
+#include <ionEngine.h>
+
+#define EIGEN_DONT_ALIGN_STATICALLY
+#include <Eigen/Dense>
+
+
+Eigen::MatrixXd RandomMatrix(int const Size)
+{
+	Eigen::MatrixXd ret;
+	ret.resize(Size, Size);
+
+	for (int i = 0; i < Size; ++ i)
+	{
+		for (int j = 0; j < Size; ++ j)
+		{
+			ret(i, j) = nrand();
+		}
+	}
+
+	return ret;
+}
+
+Eigen::VectorXd RandomVector(int const Size)
+{
+	Eigen::VectorXd ret;
+	ret.resize(Size);
+
+	for (int i = 0; i < Size; ++ i)
+	{
+		ret(i) = nrand();
+	}
+
+	return ret;
+}
+
+int const Attempts = 11;
+int const Trials = 10000;
+
+double Elapsed(LARGE_INTEGER start, LARGE_INTEGER stop, LARGE_INTEGER frequency)
+{
+	return (double) (stop.QuadPart - start.QuadPart) / (double) (frequency.QuadPart);
+}
+
+int main()
+{
+	LARGE_INTEGER frequency, start, stop;
+	QueryPerformanceFrequency(&frequency);
+
+	static int const Size = 100;
+	Eigen::MatrixXd const A = RandomMatrix(Size);
+	Eigen::MatrixXd const B = RandomMatrix(Size);
+	Eigen::MatrixXd const C = RandomMatrix(Size);
+	Eigen::MatrixXd const D = RandomMatrix(Size);
+	Eigen::MatrixXd const E = RandomMatrix(Size);
+
+	Eigen::VectorXd const x = RandomVector(Size);
+	Eigen::VectorXd y;
+
+	for (int i = 0; i < 100; ++ i)
+	{
+		y = (A * (B * (C * (D * (E * x)))));
+		y = (((((A * B) * C) * D) * E) * x);
+	}
+
+	for (int a = 0; a < Attempts; ++ a)
+	{
+		QueryPerformanceCounter(&start);
+		for (int i = 0; i < Trials; ++ i)
+		{
+			y = (A * (B * (C * (D * (E * x)))));
+		}
+		QueryPerformanceCounter(&stop);
+		double TimeA = Elapsed(start, stop, frequency);
+
+		QueryPerformanceCounter(&start);
+		for (int i = 0; i < Trials; ++ i)
+		{
+			y = (((((A * B) * C) * D) * E) * x);
+		}
+		QueryPerformanceCounter(&stop);
+		double TimeB = Elapsed(start, stop, frequency);
+
+		printf("%.9f , ", TimeA);
+		printf("%.9f\n", TimeB);
+		printf("\n");
+	}
+
+	WaitForUser();
+	return 0;
+}
