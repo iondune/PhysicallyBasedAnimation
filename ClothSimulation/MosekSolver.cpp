a89f5473219dce17713fc0e856d75311f6bad477
Ian Dunn <iondune@gmail.com>
2016-06-01 02:40:58 -0700

    Delete lots of commented code
    --HG--
    branch : rigid-body

--- ClothSimulation/MosekSolver.cpp
+++ ClothSimulation/MosekSolver.cpp
@@ -31,7 +31,6 @@ int CountNonZeroSymmetric(Eigen::MatrixXd const & m)
 }
 
 Eigen::VectorXd  MosekSolver::Solve(Eigen::MatrixXd const & Q0, Eigen::VectorXd const & c, Eigen::MatrixXd const & A, Eigen::VectorXd const & b, Eigen::VectorXd const & x0)
-//Eigen::VectorXd  MosekSolver::Solve(SSparseMatrix const & Q0, Eigen::VectorXd const & c, vector<vector<double>> const & A)
 {
 	Eigen::VectorXd ReturnValue;
 
